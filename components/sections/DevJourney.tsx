92ee28000d22d70f3afabaf510b9f71aa7a2cfb1
abhinav-barwal <abhibarwal7321@gmail.com>
2026-01-17 11:18:21 +0530

    Responsive UI Updated

--- components/sections/DevJourney.tsx
+++ components/sections/DevJourney.tsx
@@ -13,7 +13,7 @@ export default function DevJourney() {
                 <div className={`journey-info ${active ? 'readMore' : ''}`}>
                     <h3 data-aos="fade-up" data-aos-anchor-placement="top-bottom">MY DEVELOPMENT JOURNEY</h3>
                     <p className="journey-cont" data-aos="fade-up" data-aos-anchor-placement="top-bottom">I began my journey with a non-medical background, where I developed strong analytical and problem-solving skills. While pursuing my BSc, I discovered my interest in technology during which I completed a one-year computer diploma. This experience sparked my interest in web development and helped me build a strong foundation in front-end technologies. Currently, I work with HTML, CSS, JavaScript, SCSS, Bootstrap, Figma, React, and Next.js to create responsive, user-friendly, and modern web interfaces. I also use Git and GitHub for version control and project management. I focus on writing clean, structured code and continuously improving my skills by building real-world projects and learning best development practices.</p>
-                    <div data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                    <div className="readMore-btn" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
                         <button className={`secondary-btn ${active ? 'd-none' : ''}`}onClick={() => setActive(true)}>READ MORE</button>
                     </div>
                 </div>
